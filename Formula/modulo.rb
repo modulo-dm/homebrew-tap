942c7f3d68c1152912dadf1f7c9e09575d1f77c0
Steven Woolgar <steven_Woolgar@Woolsoft.com>
2018-09-27 09:06:48 -0700

    Update modulo.rb

--- Formula/modulo.rb
+++ Formula/modulo.rb
@@ -2,7 +2,7 @@ class Modulo < Formula
   desc "source-only dependency manager"
   homepage "https://github.com/modulo-dm/modulo"
   url "https://github.com/modulo-dm/modulo/archive/v0.7.0.tar.gz"
-  sha256 "b401f94ffee31a9359293222e2ec09974f9f475c"
+  sha256 "081700b316b7a7c42b18771c94835bf5b8f8fbb56d6e659f8b2ad1f669b076f0"
 
   depends_on :xcode => "9.4.1"
 
